1229c4ad8a32ec1d267a85c2f95805cce1728878
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-02-11 15:20:16 +0300

    Add project files.

---
+++ ITMO.ADO.NET-Practice01-Ex01/DBConnection.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace ITMO.ADO.NET_Practice01_Ex01
+{
+	partial class DBConnection
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.menuStripWorkWithDB = new System.Windows.Forms.MenuStrip();
+			this.ToolStripMenuItemConnectToDB = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItemDisconnectFromDB = new System.Windows.Forms.ToolStripMenuItem();
+			this.ToolStripMenuItemOpenAsyncConnectionToBD = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStripWorkWithDB.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStripWorkWithDB
+			// 
+			this.menuStripWorkWithDB.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripMenuItemConnectToDB,
+            this.ToolStripMenuItemDisconnectFromDB,
+            this.ToolStripMenuItemOpenAsyncConnectionToBD});
+			this.menuStripWorkWithDB.Location = new System.Drawing.Point(0, 0);
+			this.menuStripWorkWithDB.Name = "menuStripWorkWithDB";
+			this.menuStripWorkWithDB.Size = new System.Drawing.Size(800, 24);
+			this.menuStripWorkWithDB.TabIndex = 0;
+			this.menuStripWorkWithDB.Text = "Работа с базой данных";
+			// 
+			// ToolStripMenuItemConnectToDB
+			// 
+			this.ToolStripMenuItemConnectToDB.Name = "ToolStripMenuItemConnectToDB";
+			this.ToolStripMenuItemConnectToDB.Size = new System.Drawing.Size(122, 20);
+			this.ToolStripMenuItemConnectToDB.Text = "Подключится к БД";
+			this.ToolStripMenuItemConnectToDB.Click += new System.EventHandler(this.ToolStripMenuItemConnectToDB_Click);
+			// 
+			// ToolStripMenuItemDisconnectFromDB
+			// 
+			this.ToolStripMenuItemDisconnectFromDB.Name = "ToolStripMenuItemDisconnectFromDB";
+			this.ToolStripMenuItemDisconnectFromDB.Size = new System.Drawing.Size(120, 20);
+			this.ToolStripMenuItemDisconnectFromDB.Text = "Отключится от БД";
+			this.ToolStripMenuItemDisconnectFromDB.Click += new System.EventHandler(this.ToolStripMenuItemDisconnectFromDB_Click);
+			// 
+			// ToolStripMenuItemOpenAsyncConnectionToBD
+			// 
+			this.ToolStripMenuItemOpenAsyncConnectionToBD.Name = "ToolStripMenuItemOpenAsyncConnectionToBD";
+			this.ToolStripMenuItemOpenAsyncConnectionToBD.Size = new System.Drawing.Size(200, 20);
+			this.ToolStripMenuItemOpenAsyncConnectionToBD.Text = "Асинхронное подключение к БД";
+			this.ToolStripMenuItemOpenAsyncConnectionToBD.Click += new System.EventHandler(this.ToolStripMenuItemOpenAsyncConnectionToBD_Click);
+			// 
+			// DBConnection
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.menuStripWorkWithDB);
+			this.MainMenuStrip = this.menuStripWorkWithDB;
+			this.Name = "DBConnection";
+			this.Text = "Работа с базой данных";
+			this.menuStripWorkWithDB.ResumeLayout(false);
+			this.menuStripWorkWithDB.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStripWorkWithDB;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemConnectToDB;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemDisconnectFromDB;
+		private System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemOpenAsyncConnectionToBD;
+	}
+}
+
---
+++ ITMO.ADO.NET-Practice01-Ex01/DBConnection.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ITMO.ADO.NET_Practice01_Ex01
+{
+	public partial class DBConnection : Form
+	{
+		public DBConnection()
+		{
+			InitializeComponent();
+			this.connection.StateChange += new StateChangeEventHandler(this.Connection_StateChange);
+		}
+
+		SqlConnection connection = new SqlConnection();
+		string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True";
+
+
+
+
+		// действие по нажатию кнопки подключиться к БД
+		private void ToolStripMenuItemConnectToDB_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (connection.State != ConnectionState.Open)
+				{
+					connection.ConnectionString = connectionString;
+					connection.Open();
+					MessageBox.Show("Соединение с БД " + connection.Database + " выполненено успешно." + "\nСервер: " + connection.DataSource);
+				}
+				else
+					MessageBox.Show("Соединенеи с БД уже установлено");
+			}
+
+			catch (SqlException XcpSQL)
+			{
+				  foreach (SqlError se in XcpSQL.Errors)
+				{
+					MessageBox.Show(se.Message, "Источник ошибки: " + se.Source, MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		// действие по нажатию кнопки отключиться от БД
+		private void ToolStripMenuItemDisconnectFromDB_Click(object sender, EventArgs e)
+		{
+			if (connection.State == ConnectionState.Open)
+			{
+				connection.Close();
+				MessageBox.Show("Соединение с БД закрыто");
+			}
+			else
+				MessageBox.Show("Соединение с БД уже закрыто");
+		}
+
+
+		// действие по нажатию кнопки асинхронного подключения
+		private async void ToolStripMenuItemOpenAsyncConnectionToBD_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (connection.State != ConnectionState.Open)
+				{
+					connection.ConnectionString = connectionString;
+					await connection.OpenAsync();
+					MessageBox.Show("Соединение с БД " + connection.Database + " выполненено успешно." + "\nСервер: " + connection.DataSource);
+				}
+				else
+					MessageBox.Show("Соединенеи с БД уже установлено");
+			}
+			catch  (Exception Xcp)
+			{
+				MessageBox.Show(Xcp.Message, "Непредвиденное исключение", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		// обработчик сообытия StateChange - делает ненужные пункты меню недоступными
+		private void Connection_StateChange(object sender, StateChangeEventArgs e)
+		{
+			ToolStripMenuItemConnectToDB.Enabled = (e.CurrentState == ConnectionState.Closed);
+			ToolStripMenuItemOpenAsyncConnectionToBD.Enabled = (e.CurrentState == ConnectionState.Closed);
+			ToolStripMenuItemDisconnectFromDB.Enabled = (e.CurrentState == ConnectionState.Open);
+		}
+	}
+}
