af05ee3c96806eca410ac4260c1adfc6972c9a85
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-02-18 09:30:02 +0300

    добавлена практика 5-1

---
+++ ITMO.ADO.NET-Practice-5.1-DataSourcesWizard/DataSourcesWizard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET_Practice_5._1_DataSourcesWizard
+{
+	public partial class DataSourcesWizard : Form
+	{
+		public DataSourcesWizard()
+		{
+			InitializeComponent();
+		}
+
+		private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		{
+			this.Validate();
+			this.customersBindingSource.EndEdit();
+			this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+		}
+
+		private void DataSourcesWizard_Load(object sender, EventArgs e)
+		{
+			// TODO: This line of code loads data into the 'northwindDataSet.Orders' table. You can move, or remove it, as needed.
+			this.ordersTableAdapter.Fill(this.northwindDataSet.Orders);
+			// TODO: This line of code loads data into the 'northwindDataSet.Customers' table. You can move, or remove it, as needed.
+			this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+		}
+	}
+}
---
+++ ITMO.ADO.NET-Practice-5.1-DataSourcesWizard/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET_Practice_5._1_DataSourcesWizard
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new DataSourcesWizard());
+		}
+	}
+}
