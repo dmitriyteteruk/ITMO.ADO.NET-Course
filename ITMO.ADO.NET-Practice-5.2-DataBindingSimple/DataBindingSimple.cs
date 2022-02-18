2a4b9a35fb01d8d672572618504adc8cda01e642
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-02-18 09:45:25 +0300

    добавлено упражнение 5.2

---
+++ ITMO.ADO.NET-Practice-5.2-DataBindingSimple/DataBindingSimple.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITMO.ADO.NET_Practice_5._2_DataBindingSimple
+{
+	public partial class DataBindingSimple : Form
+	{
+
+		private BindingSource bindingSourceProducts;
+		
+		public DataBindingSimple()
+		{
+			InitializeComponent();
+		}
+
+		private void productsBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+		{
+			this.Validate();
+			this.productsBindingSource.EndEdit();
+			this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+		}
+
+		private void DataBindingSimple_Load(object sender, EventArgs e)
+		{
+			// TODO: This line of code loads data into the 'northwindDataSet.Products' table. You can move, or remove it, as needed.
+			this.productsTableAdapter.Fill(this.northwindDataSet.Products);
+
+			productsBindingSource = new BindingSource(northwindDataSet, "Products");
+
+			textBoxProductID.DataBindings.Add("Text", productsBindingSource, "ProductID");
+			textBoxProductName.DataBindings.Add("Text", productsBindingSource, "ProductName");
+
+		}
+
+		private void buttonPrevious_Click(object sender, EventArgs e)
+		{
+			productsBindingSource.MovePrevious();
+		}
+
+		private void buttonNext_Click(object sender, EventArgs e)
+		{
+			productsBindingSource.MoveNext();
+		}
+	}
+}
