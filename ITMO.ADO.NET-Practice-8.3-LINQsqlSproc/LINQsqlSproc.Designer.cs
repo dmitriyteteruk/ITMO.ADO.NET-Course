db7a00fbebc7cbbd5f6a2c3879de661eb8115682
dmitriy.teteruk <dmitriy.teteruk@gmail.com>
2022-02-21 17:36:49 +0300

    добавлено упражнение 8.3

---
+++ ITMO.ADO.NET-Practice-8.3-LINQsqlSproc/LINQsqlSproc.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace ITMO.ADO.NET_Practice_8._3_LINQsqlSproc
+{
+	partial class LINQsqlSproc
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonOrderDetails = new System.Windows.Forms.Button();
+			this.buttonOrderHistory = new System.Windows.Forms.Button();
+			this.textBoxOrderCode = new System.Windows.Forms.TextBox();
+			this.textBoxClientCode = new System.Windows.Forms.TextBox();
+			this.labelEnterOrderCode = new System.Windows.Forms.Label();
+			this.labelEnterClientCode = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonOrderDetails
+			// 
+			this.buttonOrderDetails.Location = new System.Drawing.Point(16, 88);
+			this.buttonOrderDetails.Name = "buttonOrderDetails";
+			this.buttonOrderDetails.Size = new System.Drawing.Size(159, 23);
+			this.buttonOrderDetails.TabIndex = 0;
+			this.buttonOrderDetails.Text = "Подробности заказа";
+			this.buttonOrderDetails.UseVisualStyleBackColor = true;
+			this.buttonOrderDetails.Click += new System.EventHandler(this.buttonOrderDetails_Click);
+			// 
+			// buttonOrderHistory
+			// 
+			this.buttonOrderHistory.Location = new System.Drawing.Point(383, 88);
+			this.buttonOrderHistory.Name = "buttonOrderHistory";
+			this.buttonOrderHistory.Size = new System.Drawing.Size(176, 23);
+			this.buttonOrderHistory.TabIndex = 1;
+			this.buttonOrderHistory.Text = "История заказа";
+			this.buttonOrderHistory.UseVisualStyleBackColor = true;
+			this.buttonOrderHistory.Click += new System.EventHandler(this.buttonOrderHistory_Click);
+			// 
+			// textBoxOrderCode
+			// 
+			this.textBoxOrderCode.Location = new System.Drawing.Point(16, 49);
+			this.textBoxOrderCode.Name = "textBoxOrderCode";
+			this.textBoxOrderCode.Size = new System.Drawing.Size(281, 20);
+			this.textBoxOrderCode.TabIndex = 2;
+			// 
+			// textBoxClientCode
+			// 
+			this.textBoxClientCode.Location = new System.Drawing.Point(383, 49);
+			this.textBoxClientCode.Name = "textBoxClientCode";
+			this.textBoxClientCode.Size = new System.Drawing.Size(281, 20);
+			this.textBoxClientCode.TabIndex = 3;
+			// 
+			// labelEnterOrderCode
+			// 
+			this.labelEnterOrderCode.AutoSize = true;
+			this.labelEnterOrderCode.Location = new System.Drawing.Point(13, 22);
+			this.labelEnterOrderCode.Name = "labelEnterOrderCode";
+			this.labelEnterOrderCode.Size = new System.Drawing.Size(109, 13);
+			this.labelEnterOrderCode.TabIndex = 4;
+			this.labelEnterOrderCode.Text = "Введите код заказа";
+			// 
+			// labelEnterClientCode
+			// 
+			this.labelEnterClientCode.AutoSize = true;
+			this.labelEnterClientCode.Location = new System.Drawing.Point(383, 22);
+			this.labelEnterClientCode.Name = "labelEnterClientCode";
+			this.labelEnterClientCode.Size = new System.Drawing.Size(114, 13);
+			this.labelEnterClientCode.TabIndex = 5;
+			this.labelEnterClientCode.Text = "Введите код клиента";
+			// 
+			// LINQsqlSproc
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.labelEnterClientCode);
+			this.Controls.Add(this.labelEnterOrderCode);
+			this.Controls.Add(this.textBoxClientCode);
+			this.Controls.Add(this.textBoxOrderCode);
+			this.Controls.Add(this.buttonOrderHistory);
+			this.Controls.Add(this.buttonOrderDetails);
+			this.Name = "LINQsqlSproc";
+			this.Text = "Упражнение 8.3. Использование хранимых процедур";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonOrderDetails;
+		private System.Windows.Forms.Button buttonOrderHistory;
+		private System.Windows.Forms.TextBox textBoxOrderCode;
+		private System.Windows.Forms.TextBox textBoxClientCode;
+		private System.Windows.Forms.Label labelEnterOrderCode;
+		private System.Windows.Forms.Label labelEnterClientCode;
+	}
+}
+
---
+++ ITMO.ADO.NET-Practice-8.3-LINQsqlSproc/LINQsqlSproc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Linq;
+
+namespace ITMO.ADO.NET_Practice_8._3_LINQsqlSproc
+{
+	public partial class LINQsqlSproc : Form
+	{
+		public LINQsqlSproc()
+		{
+			InitializeComponent();
+		}
+
+		Northwnd db = new Northwnd(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True");
+
+		private void buttonOrderDetails_Click(object sender, EventArgs e)
+		{
+			string param = textBoxOrderCode.Text;
+			var custquery = db.CustOrdersDetail(Convert.ToInt32(param));
+
+
+			string msg = "";
+			foreach (CustOrdersDetailResult custOrdersDetail in custquery)
+			{
+				msg = msg + custOrdersDetail.ProductName + "\n";
+			}
+			if (msg == "")
+				msg = "No results.";
+			MessageBox.Show(msg);
+
+			param = "";
+			textBoxOrderCode.Text = "";
+		}
+
+		private void buttonOrderHistory_Click(object sender, EventArgs e)
+		{
+			string param = textBoxClientCode.Text;
+			var custquery = db.CustOrderHist(param);
+			string msg = "";
+			foreach (CustOrderHistResult custOrderHist in custquery)
+			{
+				msg = msg + custOrderHist.ProductName + "\n";
+			}
+			MessageBox.Show(msg);
+			param = "";
+			textBoxClientCode.Text = "";
+		}
+	}
+}
